d265b736e23958246b4635ec236bd567843a9694
Billy Dolan <dolanwi@gmail.com>
2021-08-24 19:12:09 -0400

    incorporate more than 2 sets per day

--- collect_pics.py
+++ collect_pics.py
@@ -14,29 +14,41 @@ def collect_pics(website_url, web_prefix):
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    results = soup.find_all("div", class_="grid-container")[0]
-
-    results.append(soup.find_all("div", class_="grid-container")[1])
-
-    cards = results.find_all("div", class_="grid-card")
-
+    counter = 0
+    found = False
+    results = []
     pic_urls = []
-    for card in cards:
-        pic = card.find("img")
-        filename = (pic.get('src')).strip()
-        full_pic_url = web_prefix.format(filename)
-        file_jpeg = os.path.join("",full_pic_url.split("/")[-1])
-        file = open("pics.txt") 
-        if full_pic_url in file.read():
-            pass
-        else:
-            pic_urls.append(full_pic_url)
+
+    while found == False:
+
+
+        results = soup.find_all("div", class_="grid-container")[counter]
+
+        #results.append(soup.find_all("div", class_="grid-container")[1])
+
+        cards = results.find_all("div", class_="grid-card")
+
+        
+        for card in cards:
+            pic = card.find("img")
+            filename = (pic.get('src')).strip()
+            full_pic_url = web_prefix.format(filename)
+            file_jpeg = os.path.join("",full_pic_url.split("/")[-1])
+            file = open("pics.txt") 
+            #print(full_pic_url)
+            if full_pic_url in file.read():
+                found = True
+            else:
+                pic_urls.append(full_pic_url)
+        
+        print(found)
+        counter += 1
    #     if path.exists("card_list/" + file_jpeg):
    #         pass
    #     else:
     #        download(full_pic_url)
  #           pic_urls.append(full_pic_url)
-    print(pic_urls)
+    
     save_pic_as_txt(pic_urls)
 
     return pic_urls
